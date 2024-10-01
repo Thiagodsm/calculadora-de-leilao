0196a7f3ee0642b80fda667c54e26f1fed753830
Thiago Moreira <48686609+Thiagodsm@users.noreply.github.com>
2024-10-01 00:23:26 -0300

    Criando card com os campos de valores que serao calculados

--- src/pages/CalculosFinanceiros.tsx
+++ src/pages/CalculosFinanceiros.tsx
@@ -1,4 +1,4 @@
-import { ListFilter, File } from "lucide-react";
+import { ListFilter, File, MoreVertical, ChevronRight, ChevronLeft, Copy, TrendingDown, Wallet } from "lucide-react";
 import {
   Card,
   CardContent,
@@ -22,10 +22,13 @@ import {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "../components/ui/dropdown-menu"
+import { Pagination, PaginationContent, PaginationItem } from "../components/ui/pagination";
+import { Separator } from "../components/ui/separator";
 
 export default function CalculosFinanceiros() {
   return (
@@ -38,40 +41,39 @@ export default function CalculosFinanceiros() {
             <CardHeader className="pb-3">
               <CardTitle>Cálculos de Leilão de Extrajudicial</CardTitle>
               <CardDescription className="text-balance max-w-lg leading-relaxed">
-                Introducing Our Dynamic Orders Dashboard for Seamless
-                Management and Insightful Analysis.
+                O leilão extrajudicial é um processo que permite vender imóveis que foram dados como garantia em empréstimos que não foram pagos. O leilão é mais rápido e barato do que um processo judicial e pode oferecer imóveis a preços mais baixos do que o valor de mercado.
               </CardDescription>
             </CardHeader>
             <CardFooter>
-              <Button>Calcular</Button>
+              <Button>Faça sua simulação</Button>
             </CardFooter>
           </Card>
           <Card x-chunk="dashboard-05-chunk-1">
             <CardHeader className="pb-2">
               <CardDescription>Custos totais</CardDescription>
-              <CardTitle className="text-4xl">R$ 1,329</CardTitle>
+              <CardTitle className="text-4xl">R$ 0,00</CardTitle>
             </CardHeader>
             <CardContent>
               <div className="text-xs text-muted-foreground">
-                +25% from last week
+                0% custos totais para comprar o imóvel
               </div>
             </CardContent>
             <CardFooter>
-              <Progress value={25} aria-label="25% increase" />
+              <Progress value={0} aria-label="25% increase" />
             </CardFooter>
           </Card>
           <Card x-chunk="dashboard-05-chunk-2">
             <CardHeader className="pb-2">
-              <CardDescription>Saldo líquido</CardDescription>
-              <CardTitle className="text-4xl">$5,329</CardTitle>
+              <CardDescription>Lucro líquido</CardDescription>
+              <CardTitle className="text-4xl">R$ 0,00</CardTitle>
             </CardHeader>
             <CardContent>
               <div className="text-xs text-muted-foreground">
-                +10% from last month
+                0% lucros com a venda venda do imóvel
               </div>
             </CardContent>
             <CardFooter>
-              <Progress value={12} aria-label="12% increase" />
+              <Progress value={0} aria-label="12% increase" />
             </CardFooter>
           </Card>
         </div>
@@ -136,6 +138,185 @@ export default function CalculosFinanceiros() {
           </TabsContent>
         </Tabs>
       </div>
+      <div>
+            <Card
+              className="overflow-hidden" x-chunk="dashboard-05-chunk-4"
+            >
+              <CardHeader className="flex flex-row items-start bg-muted/50">
+                <div className="grid gap-0.5">
+                  <CardTitle className="group flex items-center gap-2 text-lg">
+                    Custos totais
+                    <Button
+                      size="icon"
+                      variant="outline"
+                      className="h-6 w-6 opacity-0 transition-opacity group-hover:opacity-100"
+                    >
+                      <Copy className="h-3 w-3" />
+                      <span className="sr-only">Copy Order ID</span>
+                    </Button>
+                  </CardTitle>
+                  <CardDescription>Custos totais com a compra do imóvel</CardDescription>
+                </div>
+                <div className="ml-auto flex items-center gap-1">
+                  <Button size="sm" variant="outline" className="h-8 gap-1">
+                    <TrendingDown className="h-3.5 w-3.5" />
+                    <span className="lg:sr-only xl:not-sr-only xl:whitespace-nowrap">
+                      Custos
+                    </span>
+                  </Button>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                      <Button size="icon" variant="outline" className="h-8 w-8" disabled={true}>
+                        <MoreVertical className="h-3.5 w-3.5" />
+                        <span className="sr-only">More</span>
+                      </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                      <DropdownMenuItem>Export</DropdownMenuItem>
+                      <DropdownMenuSeparator />
+                      <DropdownMenuItem>Trash</DropdownMenuItem>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                </div>
+              </CardHeader>
+              <CardContent className="p-6 text-sm">
+                <div className="grid gap-3">
+                  <div className="font-semibold">Demonstrativo dos custos</div>
+                  <ul className="grid gap-3">
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">
+                        Valor de arrematação 
+                      </span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">
+                        Valor de venda 
+                      </span>
+                      <span>R$ 0</span>
+                    </li>
+                  </ul>
+                  <Separator className="my-2" />
+                  <div className="font-semibold">Custos para arrematar</div>
+                  <ul className="grid gap-3">
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Comissão do leiloeiro</span>
+                      <span>0 %</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Valor da comissão</span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">ITBI</span>
+                      <span>0 %</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Valor do ITBI</span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Registro do imóvel</span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Valor desocupação (custos com advogado)</span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between font-semibold">
+                      <span className="text-muted-foreground">Total</span>
+                      <span>R$ 0</span>
+                    </li>
+                  </ul>
+                  <Separator className="my-2" />
+                  <div className="font-semibold">Custos até a venda venda</div>
+                  <ul className="grid gap-3">
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Prazo de venda</span>
+                      <span>0 mese(s)</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">IPTU mensal</span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Condominio mensal</span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between font-semibold">
+                      <span className="text-muted-foreground">Total</span>
+                      <span>R$ 0</span>
+                    </li>
+                  </ul>
+                </div>
+                <Separator className="my-4" />
+                <div className="grid gap-3">
+                  <div className="font-semibold">Custos de venda</div>
+                  <ul className="grid gap-3">
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">
+                        Comissão do corretor
+                      </span>
+                      <span>0 %</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">
+                        Valor da comissão
+                      </span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">IR sobre o ganho de capital</span>
+                      <span>15%</span>
+                    </li>
+                    <li className="flex items-center justify-between">
+                      <span className="text-muted-foreground">Valor do IR sobre o ganho de capital</span>
+                      <span>R$ 0</span>
+                    </li>
+                    <li className="flex items-center justify-between font-semibold">
+                      <span className="text-muted-foreground">Total</span>
+                      <span>R$ 0</span>
+                    </li>
+                  </ul>
+                </div>
+                <Separator className="my-4" />
+                <div className="grid gap-3">
+                  <div className="font-semibold">Custos totais</div>
+                  <dl className="grid gap-3">
+                    <div className="flex items-center justify-between">
+                      <dt className="flex items-center gap-1 text-muted-foreground">
+                        <Wallet className="h-4 w-4" />
+                        Investimento
+                      </dt>
+                      <dd>R$ 0</dd>
+                    </div>
+                  </dl>
+                </div>
+              </CardContent>
+              <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
+                <div className="text-xs text-muted-foreground">
+                  Atualizado em <time dateTime="2023-11-23">01 de Outubro de 2024</time>
+                </div>
+                <Pagination className="ml-auto mr-0 w-auto">
+                  <PaginationContent>
+                    <PaginationItem>
+                      <Button size="icon" variant="outline" className="h-6 w-6">
+                        <ChevronLeft className="h-3.5 w-3.5" />
+                        <span className="sr-only">Previous Order</span>
+                      </Button>
+                    </PaginationItem>
+                    <PaginationItem>
+                      <Button size="icon" variant="outline" className="h-6 w-6">
+                        <ChevronRight className="h-3.5 w-3.5" />
+                        <span className="sr-only">Next Order</span>
+                      </Button>
+                    </PaginationItem>
+                  </PaginationContent>
+                </Pagination>
+              </CardFooter>
+            </Card>
+          </div>
     </>
   )
 }
