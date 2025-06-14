542c080f9ffe93a635ce4aa91284b475ca4d6217
Thiagodsm <thiagosm.004@gmail.com>
2025-06-14 16:55:32 -0300

    Simplificando providers para utilizar diretamente o do Shacn

--- src/components/Layout/Provider.tsx
+++ src/components/Layout/Provider.tsx
@@ -1,39 +1,12 @@
 
 import { ThemeProvider } from '../ui/theme-provider';
-import { Dispatch, SetStateAction, createContext, useEffect, useState } from 'react';
 
-export type ThemeProps = {
-    theme: string;
-    setTheme: Dispatch<SetStateAction<string>>;
-    toggleTheme: () => void;
-}
-
-export const ThemeContext = createContext<ThemeProps | null>(null);
-
-const Providers = ({ children }: {children : React.ReactNode}) => {
-    const[theme, setTheme] = useState('dark');
-
-    const toggleTheme = () => {
-        const newTheme = theme === 'light' ? 'dark' : 'light';
-        setTheme(newTheme);
-        localStorage.setItem('theme', newTheme);
-        
-    }
-
-    useEffect(() => {
-        const themeLocalStorage = localStorage.getItem('theme');
-        if(themeLocalStorage){
-            setTheme(themeLocalStorage);
-        }
-    }, []);
-
+const Providers = ({children}: {children: React.ReactNode}) =>{
     return (
-        <ThemeContext.Provider value={{theme, setTheme, toggleTheme}}>
-            <ThemeProvider defaultTheme={'dark'}>
-                {children}
-            </ThemeProvider>
-        </ThemeContext.Provider>
-    );
+        <ThemeProvider defaultTheme='system' storageKey='vite-ui-theme'>
+            {children}
+        </ThemeProvider>
+    )
 }
 
 export default Providers;
