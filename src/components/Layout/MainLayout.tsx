ba640eb0f43a09d2d25e59f56fe9abd061c3a7f4
Thiagodsm <thiagosm.004@gmail.com>
2025-06-22 23:36:50 -0300

    Ajustando layout para lidar com a nova sidebar

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,26 +1,18 @@
 import Providers from "./Provider";
 import { TooltipProvider } from "../ui/tooltip";
-import SidebarMenu from "./SidebarMenu";
-import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
+import SidebarPage from "./SidebarPage";
 
 export default function MainLayout() {
 
   return (
     <Providers>
       <TooltipProvider>
-        <div className="flex min-h-screen w-full flex-col bg-muted/40 overflow-hidden">
-          <SidebarMenu />
-          
-          <div className="flex flex-col flex-grow sm:gap-4 sm:pl-14">
-            <main className="grid items-start gap-4 p-4">
-              <Suspense fallback={<div>Loading...</div>}>
-                <Outlet />
-              </Suspense>
-            </main>
-            <Footer />
+        <div className="flex min-h-screen w-full flex-col overflow-hidden">
+          <div className="flex-grow flex flex-col">
+            <SidebarPage />
           </div>
+          <Footer />
         </div>
       </TooltipProvider>
     </Providers>
