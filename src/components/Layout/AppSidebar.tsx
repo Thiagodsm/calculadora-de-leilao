a5d70a5304cc61e3922e225863fb3afd83ade26d
Thiagodsm <thiagosm.004@gmail.com>
2025-06-25 22:43:42 -0300

    Trocando metodo o tema do twitter

--- src/components/Layout/AppSidebar.tsx
+++ src/components/Layout/AppSidebar.tsx
@@ -42,7 +42,7 @@ const menus: Menu[] =[
 
 export function AppSidebar({ ...props }) {
   return (
-    <Sidebar {...props}>
+    <Sidebar {...props} className="bg-background text-foreground">
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
