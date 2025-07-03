bed1f7e95db2c9a2c78d2f07ccf6c221ead28d8c
Thiagodsm <thiagosm.004@gmail.com>
2025-07-02 23:10:20 -0300

    Ajustando textos da home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,13 +10,13 @@ export default function Home() {
     <>
       <div className="grid w-full auto-rows-max items-start gap-4 px-4 md:gap-8 lg:col-span-2">
         <TitlePage 
-          title="Calculadora Leilão Extrajudicial" 
-          description="Calcule de forma fácil os custos, impostos e o lucro líquido ao comprar e vender imóveis em leilão." />
+          title="Calculadora de Lucro em Imóveis de Leilão" 
+          description="Simule os custos, impostos e descubra o lucro líquido na compra e venda de imóveis arrematados em leilão extrajudicial." />
         <Card className="boder-dashed">
           <CardContent className="text-center">
           </CardContent>
           <CardHeader>
-            <CardTitle className="text-2xl text-center">Pronto para começar?</CardTitle>
+            <CardTitle className="text-2xl text-center mb-1 sm:mb-0">Pronto para começar?</CardTitle>
             <CardDescription className="text-md text-justify">
               <strong>O leilão extrajudicial é uma forma de venda de imóveis realizada fora do processo judicial</strong>. Ele ocorre quando o proprietário não paga um financiamento ou empréstimo em que o imóvel foi dado como garantia (alienação fiduciária). Nesses casos, a instituição credora pode retomar o bem e vendê-lo em leilão público para recuperar a dívida. Esse tipo de leilão costuma ser mais rápido, menos burocrático e com custos reduzidos — o que pode representar uma boa oportunidade para comprar imóveis por valores abaixo do mercado.
             </CardDescription>
@@ -28,30 +28,36 @@ export default function Home() {
           </CardHeader>
         </Card>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 w-full max-w-full">
+        <div className="w-full max-w-full">
+            <p className="text-muted-foreground text-justify">
+              A seguir, veja os principais pontos considerados nos cálculos:
+            </p>
+          </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-1 w-full max-w-full">
 
           <CardHomeDescription 
-            title="Cálculo do lucro líquido" 
-            description="Receita da venda - (Custos + IR + Taxas)" 
-            text="Simule o retorno de um imóvel considerando arrematação, reformas, impostos e comissão."
+            title="Lucro Líquido da Operação" 
+            description="Lucro = Receita de Venda - (Total de Custos + IR + Comissão do Leiloeiro)" 
+            text="Calcula o retorno real da operação, considerando desde o valor pago no leilão até os custos finais com impostos e taxas. Ideal para saber se o negócio vale a pena."
           />
 
           <CardHomeDescription 
-            title="Imposto de Renda" 
-            description="Receita da venda - (Custos + IR + Taxas)" 
+            title="Cálculo do Imposto de Renda (Ganho de Capital)" 
+            description="IR = 15% sobre o lucro (com deduções permitidas por lei)" 
             text="O sistema calcula o IR sobre ganho de capital considerando despesas dedutíveis."
           />
 
           <CardHomeDescription 
-            title="Saldo devedor e parcelas pagas (compra financiada)" 
-            description="Calcula os valor a ser pago em parcelas considerando sistemas SAC ou Price" 
+            title="Simulação de Financiamento (SAC e Price)" 
+            description="Calcula valor total pago + saldo devedor final" 
             text="Com base nos parametros passados o sistema calcula o valor gastos com as parcelas e saldo devedor a ser quitado na venda."
           />
 
           <CardHomeDescription 
-            title="Valores gastos até pós arrematação até a venda do imóvel" 
-            description="IPTU + Condominio + Total em parcelas" 
-            text="Resumo dos custos totais incorridos desde a arrematação até a venda do imóvel, incluindo taxas, reformas, impostos e demais despesas operacionais."
+            title="Despesas Pós-Arrematação até a Venda" 
+            description="Custos fixos: IPTU + Condomínio + Parcelas Pagas + Outras Despesas" 
+            text="Apresenta um resumo dos custos recorrentes e extraordinários entre o arremate e a revenda. Essencial para entender a real rentabilidade."
           />
         </div>
       </div>
